6863619e7e784a84e6b3e3f7442899e02a30ff4f
HanUseArch <wooyeophan@gmail.com>
2022-01-19 22:09:29 +0900

    Trigger and manager

---
+++ GangsterHamster/Assets/001_Scripts/Objects/Interfaces/ITrigger.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Obejcts.Trigger
+{
+    /// <summary>
+    /// 트리거 관리 용
+    /// </summary>
+    public interface ITrigger
+    {
+        /// <summary>
+        /// 구별용
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 작동 여부
+        /// </summary>
+        public bool Activated { get; set; }
+
+        /// <summary>
+        /// 트리거 작동 시 호출<br/>
+        /// OnTrigger(작동시킨 오브젝트);
+        /// </summary>
+        public Action<UnityEngine.GameObject> OnTrigger { get; set; }
+    }
+}
---
+++ GangsterHamster/Assets/001_Scripts/Objects/Trigger/TriggerObject.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obejcts.Trigger
+{
+    /// <summary>
+    /// 트리거 오브젝트
+    /// </summary>
+    abstract public class TriggerObject : MonoBehaviour, ITrigger
+    {
+        public int ID { get; set; } = -1;
+        public bool Activated { get; set; } = false;
+        public Action<GameObject> OnTrigger { get; set; }
+
+        protected virtual void Awake()
+        {
+            TriggerManager.Instance.AddTrigger(this);
+        }
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            if(!Activated) return;
+
+            if(this.ID == -1)
+            {
+                Log.Debug.Log($"TriggerObject > 등록되지 않은 트리거 {gameObject.name}", Log.LogLevel.Error);
+                return;
+            }
+
+            OnTrigger(other.gameObject);
+        }
+
+    }
+}
