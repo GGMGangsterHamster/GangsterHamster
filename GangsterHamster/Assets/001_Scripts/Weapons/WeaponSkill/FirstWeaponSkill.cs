9b4b6451b44fd3bb63fe1f0cab4d1cb87bea638c
KMJ1324 <eppurdream@gmail.com>
2022-02-03 15:17:02 +0900

    무기가 벽을 뚫어서 수정함

--- GangsterHamster/Assets/001_Scripts/Weapons/WeaponSkill/FirstWeaponSkill.cs
+++ GangsterHamster/Assets/001_Scripts/Weapons/WeaponSkill/FirstWeaponSkill.cs
@@ -14,6 +14,8 @@ public class FirstWeaponSkill : MonoBehaviour
 
     private List<IInteractableObject> objList;
 
+    private Vector3 shotVec = Vector3.zero;
+
     private void Start()
     {
         _myRigid = GetComponent<Rigidbody>();
@@ -21,11 +23,6 @@ public class FirstWeaponSkill : MonoBehaviour
 
         objsParent = transform.GetChild(0);
         objList = new List<IInteractableObject>();
-    }
-
-    private void Update()
-    {
-
     }
 
     /// <summary>
@@ -75,6 +72,7 @@ public class FirstWeaponSkill : MonoBehaviour
         if(transform.parent == null)
         {
             _myRigid.useGravity = false;
+            _myRigid.velocity = Vector2.zero;
 
             StopAllCoroutines();
 
@@ -101,16 +99,15 @@ public class FirstWeaponSkill : MonoBehaviour
 
         if (transform.parent == null)
         {
-            if(collision.transform.TryGetComponent(out IInteractableObject outII))
+            if(collision.transform.TryGetComponent(out IInteractableObject outII)) // 만약 TypeObj이라면
             {
                 StopAllCoroutines();
 
                 outII.Collision(objsParent.gameObject);
 
-                _myRigid.velocity = Vector3.zero;
-
-                if((outII as ObjectA) != null) // Type이 A일 경우
+                if((outII as ObjectA) != null) // Type이 A일 경우 무기의 중력을 없애고 움직임을 얼린다
                 {
+                    _myRigid.velocity = Vector3.zero;
                     _myRigid.useGravity = false;
                     _myCol.isTrigger = true;
 
@@ -119,15 +116,16 @@ public class FirstWeaponSkill : MonoBehaviour
 
                     objList.Add(outII);
                 }
-                else if((outII as ObjectB) != null) // Type이 B일 경우
+                else if((outII as ObjectB) != null) // Type이 B일 경우 붙어있던 모든 오브젝트를 떼어내고 중력을 활성화하며 움직임도 활성화 한다
                 {
                     ClearList();
 
+                    _myRigid.velocity = shotVec / shotSpeed;
                     _myRigid.useGravity = true;
                     transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
                 }
             }
-            else if(collision.transform.CompareTag("PLAYER_BASE"))
+            else if(collision.transform.CompareTag("PLAYER_BASE")) // 플레이어라면 오른손에 무기가 돌아오게 한다
             {
                 ComeBack(GameObject.Find("RightHand").transform);
             }
@@ -150,7 +148,9 @@ public class FirstWeaponSkill : MonoBehaviour
 
         while(true)
         {
-            transform.position += dir * Time.deltaTime * shotSpeed;
+            shotVec = dir * shotSpeed;
+            
+            transform.position += shotVec * Time.deltaTime;
 
             yield return null;
         }
@@ -171,11 +171,13 @@ public class FirstWeaponSkill : MonoBehaviour
         bool isFollow = false;
         bool endFollowCheck = false;
 
+        Collider[] cols;
+
         while(true)
         {
             if(!endFollowCheck)
             {
-                if (Input.GetKey(KeyCode.Mouse1))
+                if (Input.GetKey(KeyCode.Mouse1)) // 1.5초동안 우클릭을 누르고 있다면 플레이어를 지속적으로 따라감
                 {
                     waitFollowTime -= Time.deltaTime;
 
@@ -185,7 +187,7 @@ public class FirstWeaponSkill : MonoBehaviour
                         endFollowCheck = true;
                     }
                 }
-                else
+                else if(Input.GetKeyUp(KeyCode.Mouse1)) // 중간에 우클릭을 떼어버리면 가던 방향으로 계속 간다
                 {
                     endFollowCheck = true;
                 }
@@ -198,7 +200,21 @@ public class FirstWeaponSkill : MonoBehaviour
 
             transform.position += dir * Time.deltaTime * shotSpeed;
 
-            if(Vector3.Distance(distTrm.position, transform.position) <= 1f)
+            cols = Physics.OverlapSphere(transform.position + dir * Time.deltaTime * shotSpeed, transform.localScale.x * 0.5f);
+
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if (cols[i].TryGetComponent(out IInteractableObject outII))
+                {
+                    if ((outII as ObjectB) != null) // Type이 B일 경우
+                    {
+                        transform.position -= dir * Time.deltaTime * shotSpeed;
+                        yield break;
+                    }
+                }
+            }
+
+            if (Vector3.Distance(distTrm.position, transform.position) <= 1f) // 플레이어와 거리가 1 이하라면 오른손으로 돌아오게 한다
             {
                 ComeBack(rightHandTrm);
                 yield break;
