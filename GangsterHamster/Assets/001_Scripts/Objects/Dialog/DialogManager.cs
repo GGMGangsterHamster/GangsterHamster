9e3f22c98d71bf408b42645246c31b07467e4389
HanUseGentoo <wooyeophan@gmail.com>
2022-02-03 16:48:09 +0900

    다이얼로그 코드

--- GangsterHamster/Assets/001_Scripts/Objects/Dialog/DialogManager.cs
+++ GangsterHamster/Assets/001_Scripts/Objects/Dialog/DialogManager.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using Objects.UI.VO;
 
 // 다이얼로그 구조
 /*
@@ -11,13 +13,13 @@ using UnityEngine.UI;
                 index: 0,
                 message: "여기에 다이얼로그 입력."
                 iconID: 0,
-                customCallbackId: -1,
+                customCallbackID: -1,
             },
             {
                 index: 1,
                 message: "전기는 국산이지만 원료는 수입입니다."
                 iconID: 1,
-                customCallbackId: 1,
+                customCallbackID: 1,
             }
         ]
     },
@@ -34,19 +36,72 @@ namespace Objects.UI
 {
     public class DialogManager : MonoSingleton<DialogManager>
     {
-        [SerializeField] private DialogUI dialogUI;
-        
+        [SerializeField] private DialogUI _dialogUI;
+
+        // 현재 다이얼로그 정보
+        private int _currentDialogID = -1;
+        private int _currentDialogIndex = -1;
+        public int CurrentDialogID => _currentDialogID;
+        public int CurrentDialogIndex => _currentDialogIndex;
+
+        private DialogVO _dialog; // 다이얼로그
+        private DialogDataVO _currentDialog; // 현재 다이얼로그
+
 
         private void Awake()
         {
             string dialogJson = (Resources.Load("Dialog") as TextAsset).text;
             if(dialogJson == null) Logger.Log("DialogManager > 다이얼로그를 찾을 수 없습니다.", LogLevel.Fatal);
             Debug.Log(dialogJson);
+
+            _dialog = JsonUtility.FromJson<DialogVO>(dialogJson);
         }
 
+        /// <summary>
+        /// 다이얼로그를 띄웁니다
+        /// </summary>
+        /// <param name="id">띄울 다이얼로그 아이디</param>
+        public void Show(int id = -1)
+        {
+            if(CurrentDialogID != -1) {
+                Logger.Log($"DialogManager > 이미 다이얼로그 id:{CurrentDialogID} 를 실행 중.");
+            }
 
-        public void Show(int id) { }
-        public void Next() { }
-        public void Close() { }
+            _currentDialog = _dialog.dialogs.Find(e => e.id == id);
+
+            if(_currentDialog == null) { // null 체크
+                Logger.Log($"DialogManager > 요청한 id:{id} 를 찾을 수 없습니다.");
+                return;
+            }
+
+            _currentDialogID = id;
+            _currentDialogIndex = 0;
+
+            _dialogUI.Show(_currentDialog.dialog[id].message, null);
+            // _dialogUI.Show(_currentDialog.dialog[id], _currentDialog.dialog[id].iconID);
+        }
+
+        /// <summary>
+        /// 다음 다이얼로그로 이동합니다.
+        /// </summary>
+        public void Next()
+        {
+            if(_currentDialog.dialog.Count <= _currentDialogIndex + 1) {
+                Close();
+            }
+            else {
+                Show(++_currentDialogIndex);
+            }
+        }
+        
+        /// <summary>
+        /// 다이얼로그를 닫습니다.
+        /// </summary>
+        public void Close()
+        {
+            _currentDialogID = -1;
+            _dialogUI.Close();
+        }
+
     }
 }
--- GangsterHamster/Assets/001_Scripts/Objects/Dialog/DialogUI.cs
+++ GangsterHamster/Assets/001_Scripts/Objects/Dialog/DialogUI.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Objects.UI
 {
     public class DialogUI : MonoBehaviour
     {
+        [SerializeField] Text _text;
+        [SerializeField] Image _icon;
 
+        public void Show(string message, Image icon)
+        {
+            _text.text = message;
+            _icon = icon;
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
