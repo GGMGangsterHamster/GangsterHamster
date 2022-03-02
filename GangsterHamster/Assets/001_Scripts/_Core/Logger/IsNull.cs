49cdfef08a41d90176767bd77af76d2138c750dc
HanUseGentoo <wooyeophan@gmail.com>
2022-03-02 19:58:55 +0900

    편한 NullCheker 만들어둠

---
+++ GangsterHamster/Assets/001_Scripts/_Core/Logger/IsNull.cs
@@ -0,0 +1,28 @@
+using System;
+
+static class NULL
+{
+    /// <summary>
+    /// Null checker for your convenience<br/>
+    /// Only checks inside unity editor
+    /// </summary>
+    /// <param name="obj">Object that you want to check if it is null</param>
+    /// <param name="whenNullAction">called when null<br/>Leaves error log when null</param>
+    /// <param name="whenNotNullAction">called when not null</param>
+    static public void Check(object obj, Action whenNullAction = null, Action whenNotNullAction = null)
+    {
+        #if UNITY_EDITOR
+
+        if(obj == null) {
+            if(whenNullAction != null) {
+                whenNullAction();
+            } else {
+                Logger.Log($"Type: {obj.GetType()} is null.", LogLevel.Error);
+            }
+        } else {
+            whenNotNullAction?.Invoke();
+        }
+
+        #endif
+    }
+}
