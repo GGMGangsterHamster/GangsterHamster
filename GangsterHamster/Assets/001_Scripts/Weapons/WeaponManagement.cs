81b6bf011b3282c10e1065fe15ccf90f9e7d9ef7
KMJ1324 <eppurdream@gmail.com>
2022-03-03 15:52:11 +0900

    무기 바꾸는 코드 80% 작성 이후 더 작성할거임

--- GangsterHamster/Assets/001_Scripts/Weapons/WeaponManagement.cs
+++ GangsterHamster/Assets/001_Scripts/Weapons/WeaponManagement.cs
@@ -19,15 +19,67 @@ namespace Commands.Weapon
         [SerializeField] private FirstWeaponSkill _firstWeaponSkill;
         [SerializeField] private SecondWeaponSkill _secondWeaponSkill;
 
+        private const int firstWeaponNumber = 1;
+        private int lastWeaponNumber = 0; // 추후에 무기를 얻으면 얻을수록 늘어난다
+
         private void Awake()
         {
-            curWeaponNumber = 1;
+            curWeaponNumber = -1;
 
             _weaponDict.Add(1, new FirstWeapon(gameObject, _firstWeaponSkill));
             _weaponDict.Add(2, new SecondWeapon(gameObject, _secondWeaponSkill));
         }
+
+        private void Update()
+        {
+            if(curWeaponNumber != -1 && lastWeaponNumber != 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    if(lastWeaponNumber >= 1)
+                    {
+                        curWeaponNumber = 1;
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    if(lastWeaponNumber >= 2)
+                    {
+                        curWeaponNumber = 2;
+                    }
+                }
+
+                if (Input.mouseScrollDelta.y > 0) // 스크롤을 위로 굴렸을 때
+                {
+                    if (_weaponDict.TryGetValue(curWeaponNumber + 1, out WeaponCommand wc))
+                    {
+                        curWeaponNumber++;
+                    }
+                    else
+                    {
+                        curWeaponNumber = firstWeaponNumber;
+                    }
+                }
+                else if (Input.mouseScrollDelta.y < 0) // 스크롤을 아래로 굴렸을 때
+                {
+                    if (_weaponDict.TryGetValue(curWeaponNumber - 1, out WeaponCommand wc))
+                    {
+                        curWeaponNumber--;
+                    }
+                    else
+                    {
+                        curWeaponNumber = lastWeaponNumber;
+                    }
+                }
+            }
+
+            Debug.Log(curWeaponNumber);
+        }
+
         public void MouseLeft()
         {
+            if (curWeaponNumber == -1) return;
+
             if(_weaponDict.TryGetValue(curWeaponNumber, out outwc))
             {
                 outwc.Left();
@@ -36,6 +88,8 @@ namespace Commands.Weapon
 
         public void MouseRight()
         {
+            if (curWeaponNumber == -1) return;
+
             if (_weaponDict.TryGetValue(curWeaponNumber, out outwc))
             {
                 outwc.Right();
@@ -44,7 +98,9 @@ namespace Commands.Weapon
 
         public void R()
         {
-            if(_weaponDict.TryGetValue(curWeaponNumber, out outwc))
+            if (curWeaponNumber == -1) return;
+
+            if (_weaponDict.TryGetValue(curWeaponNumber, out outwc))
             {
                 outwc.Reset();
             }
