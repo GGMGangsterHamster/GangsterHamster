659f54e022a6a7b6236a14233b07db5310d0dbb3
HanUseGentoo <wooyeophan@gmail.com>
2022-03-22 11:18:04 +0900

    MonoSingleton 이제 없으면 만들어줌

--- GangsterHamster/Assets/001_Scripts/_Core/MonoSingleton.cs
+++ GangsterHamster/Assets/001_Scripts/_Core/MonoSingleton.cs
@@ -17,12 +17,28 @@ abstract public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
                _instance = objs[0];
                if (objs.Length > 1)
                {
-                  Logger.Log($"{_instance.GetType()} Found more than one.", LogLevel.Error);
+                  Logger.Log($"{_instance.GetType()} Found more than one.",
+                           LogLevel.Error);
+                  for (int i = 1; i < objs.Length; ++i) // 중복 오브젝트 삭제
+                  {
+                     Destroy(objs[i]);
+                  }
+                  GC.Collect();
+
                }
+
+               DontDestroyOnLoad(_instance.gameObject);
             }
-            else
-            {
-               Logger.Log($"FIXME: \"NAME\" Does not exist.", LogLevel.Fatal);
+            else // 없으면 새로 만듬
+            { 
+               Logger.Log($"{typeof(T)} Does not exist, Creating.",
+                        LogLevel.Warning);
+
+               GameObject obj = new GameObject(typeof(T).ToString());
+               obj.AddComponent<T>();
+
+               _instance = obj.GetComponent<T>();
+               DontDestroyOnLoad(_instance.gameObject);
             }
             
          }
