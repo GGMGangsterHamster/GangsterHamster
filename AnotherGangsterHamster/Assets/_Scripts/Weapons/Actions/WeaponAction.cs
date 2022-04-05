2319c7fe6e0c05d1aa64d2aab222b3ae80ceedd4
KMJ1324 <eppurdream@gmail.com>
2022-04-05 22:25:52 +0900

    그 하던거 일단 올리기

--- AnotherGangsterHamster/Assets/_Scripts/Weapons/Actions/WeaponAction.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Weapons/Actions/WeaponAction.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Weapons.Actions
 {
     // 이너시오, 그랜드, 그래비토 3가지의 무기들을 관리하기 위한 부모 클래스이며
-    // 공통된 기능들을 정리 해놓기도 하였다.
+    // 공통된 기능들을 함수로 정리 해놓기도 하였다.
     public class WeaponAction : MonoBehaviour
     {
         public WeaponEnum _weaponEnum; // 상속받은 무기의 종류
