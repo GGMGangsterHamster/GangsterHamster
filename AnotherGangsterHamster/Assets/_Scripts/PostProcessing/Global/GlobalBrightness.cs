274cc5a1ab5d3e5d7a97bd630c9696cd08c20c99
HanUseGentoo <wooyeophan@gmail.com>
2022-04-06 00:42:35 +0900

    Effects 기반 클레스

--- AnotherGangsterHamster/Assets/_Scripts/PostProcessing/Global/GlobalBrightness.cs
+++ AnotherGangsterHamster/Assets/_Scripts/PostProcessing/Global/GlobalBrightness.cs
@@ -1,4 +1,11 @@
-namespace PostProcessing.Global
-{
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
+namespace PostProcessing.Global
+{  
+   public class GlobalBrightness : Effects<ColorAdjustments>
+   {
+      
+   }
 }
