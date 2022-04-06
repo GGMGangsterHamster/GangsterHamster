247264b4122b69459f887d9d0f825a1e2b39887d
HanUseGentoo <wooyeophan@gmail.com>
2022-04-06 21:05:44 +0900

    제네릭 풀메니저

--- AnotherGangsterHamster/Assets/_Scripts/Sound/SoundManager.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Sound/SoundManager.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sound
 {
-   public class SoundManager : MonoBehaviour
+   public class SoundManager : MonoSingleton<SoundManager>
    {
-      
+      private List<AudioSource> _pool = new List<AudioSource>();
+
+
+      protected override void Awake()
+      {
+         base.Awake();
+         
+      }
    }
 }
