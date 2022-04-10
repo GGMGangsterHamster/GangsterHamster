21eb03accbbb8a3c4b1160d1b6acfe748e04929c
HanUseGentoo <wooyeophan@gmail.com>
2022-04-10 22:09:15 +0900

    아마도 문법에러를 하나 고침

--- AnotherGangsterHamster/Assets/_Scripts/PostProcessing/Global/GlobalBrightness.cs
+++ AnotherGangsterHamster/Assets/_Scripts/PostProcessing/Global/GlobalBrightness.cs
@@ -4,7 +4,7 @@ using UnityEngine.Rendering.HighDefinition;
 
 namespace PostProcessing.Global
 {  
-   public class GlobalBrightness : Effects<ColorAdjustments>
+   public class GlobalBrightness : Effects<Exposure>
    {
       
    }
