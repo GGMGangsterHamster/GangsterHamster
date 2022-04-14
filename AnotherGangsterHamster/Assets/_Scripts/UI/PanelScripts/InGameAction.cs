eaae7466354a639e654ebdd722d348ae0d97263b
KMJ1324 <eppurdream@gmail.com>
2022-04-14 13:53:46 +0900

    ATypeObj 에임포인트에 올라가면 색깔 바뀌게 만들ㅇ

--- AnotherGangsterHamster/Assets/_Scripts/UI/PanelScripts/InGameAction.cs
+++ AnotherGangsterHamster/Assets/_Scripts/UI/PanelScripts/InGameAction.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.PanelScripts
 {
     public class InGameAction : UIAction
     {
+        private Transform _mainCameraTransform;
+
+        public Image aimImage;
+
+        private Transform MainCameraTransform
+        {
+            get
+            {
+                if(_mainCameraTransform == null)
+                {
+                    _mainCameraTransform = Camera.main.transform;
+                }
+
+                return _mainCameraTransform;
+            }
+        }
 
         public override void ActivationActions()
         {
@@ -26,6 +43,18 @@ namespace UI.PanelScripts
             {
                 Utils.UnlockCursor();
                 UIManager.Instance.ActivationPanel(UIPanels.Pause);
+            }
+
+            if (UnityEngine.Physics.Raycast(MainCameraTransform.position, MainCameraTransform.forward, out RaycastHit hit))
+            {
+                if (hit.transform.tag == "ATYPEOBJECT")
+                {
+                    aimImage.color = Color.red;
+                }
+                else
+                {
+                    aimImage.color = Color.black;
+                }
             }
         }
     }
