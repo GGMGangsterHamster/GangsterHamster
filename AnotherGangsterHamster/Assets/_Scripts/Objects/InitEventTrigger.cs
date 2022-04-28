614d9784fbfe80823d6dc643000e2ce1c8bb4c3f
HanUseGentoo <wooyeophan@gmail.com>
2022-04-28 16:27:02 +0900

    씬 나눔

---
+++ AnotherGangsterHamster/Assets/_Scripts/Objects/InitEventTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Objects
+{
+   public class InitEventTrigger : MonoBehaviour
+   {
+      private void Start()
+      {
+         if (TryGetComponent<ICollisionEventable>(out var action))
+         {
+            action.Active(this.gameObject);
+         }
+         else
+         {
+            Logger.Log($"{gameObject.name} 에서 아무런 이벤트를 찾지 못함.",
+                  LogLevel.Warning);
+         }
+      }
+   }
+}
