e440bbba764eac9e1779b4a67b15784174ce6bcd
HanUsingWindows <wooyeophan@gmail.com>
2022-05-08 23:55:58 +0900

    신 리로드 했을 때 마우스 락 검

--- AnotherGangsterHamster/Assets/_Scripts/_Core/Initialize/InitScripts/LockCursor.cs
+++ AnotherGangsterHamster/Assets/_Scripts/_Core/Initialize/InitScripts/LockCursor.cs
@@ -2,7 +2,7 @@ namespace _Core.Initialize.InitScripts
 {
    public class LockCursor : InitBase
    {
-      public override RunLevel RunLevel => RunLevel.GAME_START;
+      public override RunLevel RunLevel => RunLevel.SCENE_LOAD;
 
       public override void Call()
       {
