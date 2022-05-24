ad5d90357a89ba96e236728540cd0d4956e13d50
KMJ1324 <eppurdream@gmail.com>
2022-05-24 22:00:12 +0900

    하던거

--- AnotherGangsterHamster/Assets/_Creepy_Cat/Common Scripts/Requirements/ButtonCountRequirement.cs
+++ AnotherGangsterHamster/Assets/_Creepy_Cat/Common Scripts/Requirements/ButtonCountRequirement.cs
@@ -1,4 +1,5 @@
 using Objects;
+using Objects.StageObjects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,10 @@ public class ButtonCountRequirement : MonoBehaviour
 {
     [SerializeField] private List<TriggerInteractableObject> countCheckList 
         = new List<TriggerInteractableObject>();
+
+    [SerializeField]
+    private List<InteractionButton> icountCheckList
+       = new List<InteractionButton>();
 
     private int _count = 0;
     public bool Checked
@@ -17,13 +22,21 @@ public class ButtonCountRequirement : MonoBehaviour
 
             for(int i = 0; i < countCheckList.Count; i++)
             {
-                if(countCheckList[i]._activated)
+                if(countCheckList[i].Activated) 
                 {
                     _count++;
                 }
             }
 
-            return _count == countCheckList.Count;
+            for (int i = 0; i < icountCheckList.Count; i++)
+            {
+                if (icountCheckList[i].Activated)
+                {
+                    _count++;
+                }
+            }
+
+            return _count == countCheckList.Count + icountCheckList.Count;
         }
     }
 
