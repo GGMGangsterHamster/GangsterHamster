6e1695547eb3811194a58c200a179eca8aef4386
HanUseGentoo <wooyeophan@gmail.com>
2022-06-20 20:02:01 +0900

    AType 헥사곤 이펙트 코드 나눔

--- AnotherGangsterHamster/Assets/_Scripts/Effects/Shaders/TypeObject/Hexagon/HexagonBrightnessEffect.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Effects/Shaders/TypeObject/Hexagon/HexagonBrightnessEffect.cs
@@ -2,58 +2,25 @@ using Objects;
 using Tween;
 using UnityEngine;
 
-namespace Effects.Shaders.TypeObject
-{   
-   [RequireComponent(typeof(TriggerInteractableObject))]
-   public class ATypeHexagon : MonoBehaviour, ICollisionEventable
+namespace Effects.Shaders.TypeObject.Hexagon
+{
+   [RequireComponent(typeof(ATypeHexagon), typeof(TriggerInteractableObject))]
+   public class HexagonBrightnessEffect : MonoBehaviour, ICollisionEventable
    {
-      private const string WALLHEIGHT = "_WallHeight";
-      private const string WALLSCALE = "_WallScale";
-      private const string BLOOM = "_Bloom";
-      private const string FADEINGSPEED = "_FadingSpeed";
-      private const string MAXALPHA = "_MaxAlpha";
-      private const string MINALPHA = "_MinAlpha";
-
       private readonly float _maxAlpha = 1.0f;
       private readonly float _minAlpha = 0.0f;
       private readonly float _defMaxAlpha = 0.25f;
 
-      public float wallScale = 1.0f;
-      public float bloom = 1.0f;
       public float collisionAlphaEffectDuration = 1.0f;
 
-      public Vector3Int considerDirection = Vector3Int.down;
-      public Transform baseTransform = null;
-
-      private Material _mat;
-
-      private float _height;
       private float _curAlpha;
       private Coroutine _curRoutine;
       private Coroutine _maxAlphaRoutine;
-
+      private Material _mat;
 
       private void Awake()
       {
          _mat = GetComponent<Renderer>().material;
-         _height = 0;
-
-         _height += (considerDirection.x != 0 ?
-            Mathf.Abs(transform.position.x - baseTransform.position.x) : 0);
-
-         _height += (considerDirection.y != 0 ?
-            Mathf.Abs(transform.position.y - baseTransform.position.y) : 0);
-
-         _height += (considerDirection.z != 0 ?
-            Mathf.Abs(transform.position.z - baseTransform.position.z) : 0);
-
-         _mat.SetFloat(WALLHEIGHT, _height / wallScale);
-         _mat.SetFloat(WALLSCALE, 1.0f / wallScale);
-      }
-
-      private void Update()
-      {
-         _mat.SetFloat(BLOOM, bloom);
       }
 
       public void Active(GameObject other)
@@ -69,18 +36,18 @@ namespace Effects.Shaders.TypeObject
 
          _curRoutine = ValueTween.To(this, () => {
             _curAlpha += step * Time.deltaTime;
-            _mat.SetFloat(MINALPHA, _curAlpha);
+            _mat.SetFloat(ATypeHexagon.MINALPHA, _curAlpha);
          }, () => _curAlpha >= _maxAlpha, () => {
             _curAlpha = _maxAlpha;
-            _mat.SetFloat(MINALPHA, _maxAlpha);
+            _mat.SetFloat(ATypeHexagon.MINALPHA, _maxAlpha);
             _curRoutine = null;
          });
 
          _maxAlphaRoutine = ValueTween.To(this, () => {
             curMaxAlpha += step * Time.deltaTime;
-            _mat.SetFloat(MAXALPHA, curMaxAlpha);
+            _mat.SetFloat(ATypeHexagon.MAXALPHA, curMaxAlpha);
          }, () => curMaxAlpha >= _maxAlpha, () => {
-            _mat.SetFloat(MAXALPHA, _maxAlpha);
+            _mat.SetFloat(ATypeHexagon.MAXALPHA, _maxAlpha);
             _maxAlphaRoutine = null;
          });
       }
@@ -98,18 +65,18 @@ namespace Effects.Shaders.TypeObject
 
          _curRoutine = ValueTween.To(this, () => {
             _curAlpha -= step * Time.deltaTime;
-            _mat.SetFloat(MINALPHA, _curAlpha);
+            _mat.SetFloat(ATypeHexagon.MINALPHA, _curAlpha);
          }, () => _curAlpha <= _minAlpha, () => {
             _curAlpha = _minAlpha;
-            _mat.SetFloat(MINALPHA, _minAlpha);
+            _mat.SetFloat(ATypeHexagon.MINALPHA, _minAlpha);
             _curRoutine = null;
          });
 
          _maxAlphaRoutine = ValueTween.To(this, () => {
             curMaxAlpha -= step * Time.deltaTime;
-            _mat.SetFloat(MAXALPHA, curMaxAlpha);
+            _mat.SetFloat(ATypeHexagon.MAXALPHA, curMaxAlpha);
          }, () => curMaxAlpha <= _defMaxAlpha, () => {
-            _mat.SetFloat(MAXALPHA, _defMaxAlpha);
+            _mat.SetFloat(ATypeHexagon.MAXALPHA, _defMaxAlpha);
             _maxAlphaRoutine = null;
          });
       }
