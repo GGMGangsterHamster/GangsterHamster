4a40c24e19b1b55f4f845915951ac01822ed78df
HanUseGentoo <wooyeophan@gmail.com>
2022-07-07 01:54:32 +0900

    버튼 수정과 맵 수정

--- AnotherGangsterHamster/Assets/_Scripts/Objects/StageObjects/ButtonActivatedChecker.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Objects/StageObjects/ButtonActivatedChecker.cs
@@ -17,7 +17,7 @@ namespace Objects.StageObjects
       {
          for (int i = 0; i < Buttons.Count; ++i)
          {
-            ICallbacks interactable = Buttons[i].GetComponentInChildren<ICallbacks>();
+            ICallbacks interactable = Buttons[i].GetComponent<ICallbacks>();
 
             if (interactable != null)
             {
