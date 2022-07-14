384d4ff422af4a5b7bda275f558dfd6042d1ae53
HanUseGentoo <wooyeophan@gmail.com>
2022-07-14 14:17:52 +0900

    불필요한 에셋과 파일 삭제

--- AnotherGangsterHamster/Assets/_Creepy_Cat/Common Scripts/Requirements/Requirement.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Requirement : MonoBehaviour
-{
-    // 만약 어떤 행도에 조건을 추가하고 싶다면 이 스크립트를 추가하고 
-    // 알아서 코딩하기
-}
