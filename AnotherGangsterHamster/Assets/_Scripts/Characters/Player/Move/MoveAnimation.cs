eb14fc03315ef5d5a354fe18b7cee242787f54f2
HanUseGentoo <wooyeophan@gmail.com>
2022-07-14 16:30:56 +0900

    루트모션 적용과 플레이어 이벤트 방식으로 처리함

--- AnotherGangsterHamster/Assets/_Scripts/Characters/Player/Move/MoveAnimation.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Characters/Player/Move/MoveAnimation.cs
@@ -5,6 +5,7 @@ namespace Characters.Player.Move
    public class MoveAnimation : MonoBehaviour
    {
       private Animator _animator;
+      private MoveInputHandler _moveInputHandler;
 
       const string X = "X";
       const string Y = "Y";
@@ -12,8 +13,26 @@ namespace Characters.Player.Move
       private void Awake()
       {
          _animator = GetComponentInChildren<Animator>();
+         _moveInputHandler = GetComponent<MoveInputHandler>();
+
+
+         _moveInputHandler.forward.Execute.AddListener(e => {
+            _animator.SetFloat(Y, 1.0f);
+         });
+
+         _moveInputHandler.backward.Execute.AddListener(e => {
+            _animator.SetFloat(Y, -1.0f);
+         });
+
+         _moveInputHandler.left.Execute.AddListener(e => {
+            _animator.SetFloat(X, -1.0f);
+         });
+
+         _moveInputHandler.right.Execute.AddListener(e => {
+            _animator.SetFloat(X, 1.0f);
+         });
       }
 
-      
+
    }
 }
