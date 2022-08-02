4d8e1f0cea19210e43789194f5bde52eb553c2fc
HanPCGentoo <wooyeophan@gmail.com>
2022-08-02 15:58:59 +0900

    리눅스 이슈로 라이트 다시 구움

--- AnotherGangsterHamster/Assets/_Scripts/_Core/Authority.cs
+++ AnotherGangsterHamster/Assets/_Scripts/_Core/Authority.cs
@@ -2,29 +2,29 @@ using System.Collections.Generic;
 
 public class Authority : Singleton<Authority>
 {
-   private Dictionary<string, int> _keyDictionary;
+    private Dictionary<string, int> _keyDictionary;
 
-   public Authority()
-   {
-      _keyDictionary = new Dictionary<string, int>();
-   }
+    public Authority()
+    {
+        _keyDictionary = new Dictionary<string, int>();
+    }
 
-   public int GetOwnership(string type)
-   {
-      int key = 0;
+    public int GetOwnership(string type)
+    {
+        int key = 0;
 
-      if (_keyDictionary.ContainsKey(type))
-         key = ++_keyDictionary[type];
-      else
-         _keyDictionary.Add(type, key);
+        if (_keyDictionary.ContainsKey(type))
+            key = ++_keyDictionary[type];
+        else
+            _keyDictionary.Add(type, key);
 
-      return key;
-   }
+        return key;
+    }
 
-   public bool CheckOwnership(string type, int key)
-   {
-      if (!_keyDictionary.ContainsKey(type))
-         return true;
-      return _keyDictionary[type] == key;
-   }
+    public bool CheckOwnership(string type, int key)
+    {
+        if (!_keyDictionary.ContainsKey(type))
+            return true;
+        return _keyDictionary[type] == key;
+    }
 }
