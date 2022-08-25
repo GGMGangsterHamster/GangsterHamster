6b637ee69f4c966057966120ed829cbc31d5acfd
HanPCGentoo <wooyeophan@gmail.com>
2022-08-25 22:05:36 +0900

    9스테이지들 잘 만듬ㄴ

--- AnotherGangsterHamster/Assets/_Scripts/Objects/InteractableObjects/BothInteractableObject.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Objects/InteractableObjects/BothInteractableObject.cs
@@ -5,68 +5,91 @@ using UnityEngine.Events;
 
 namespace Objects.InteractableObjects
 {
-   public class BothInteractableObject : InteractableObjects, IInteractable
-   {
-      // Collision에서 Normal 벡터를 빼내기 위해서 존재하는 변수
-      public Vector3 colNormalVec;
+    public class BothInteractableObject : InteractableObjects, IInteractable
+    {
+        // Collision에서 Normal 벡터를 빼내기 위해서 존재하는 변수
+        public Vector3 colNormalVec;
 
-      // 충돌 시의 Velocity
-      public Vector3 colVelocity;
+        // 충돌 시의 Velocity
+        public Vector3 colVelocity;
 
-      // 충돌 지점
-      public Vector3 colPosition;
+        // 충돌 지점
+        public Vector3 colPosition;
 
-      public bool canCollision = true;
-      public bool canTrigger = false;
-      public bool canInteract = true;
+        public bool canCollision = true;
+        public bool canTrigger = false;
+        public bool canInteract = true;
 
-      [field: SerializeField]
-      public bool CanInteractByPlayer { get; set; }
+        private bool _cancol = true;
+        private bool _canTri = false;
+        private bool _canInter = true;
 
-      protected override void Awake()
-      {
-         base.Awake();
-      }
 
-      #region Unity Collision Event
-      private void OnCollisionEnter(Collision other)
-      {
-         if (!canCollision) return;
+        [field: SerializeField]
+        public bool CanInteractByPlayer { get; set; }
 
-         colNormalVec   = other.contacts[0].normal;
-         colVelocity    = other.relativeVelocity;
-         colPosition    = other.contacts[0].point;
+        protected override void Awake()
+        {
+            base.Awake();
 
-         OnEventTrigger(other.gameObject);
-      }
+            _cancol = canCollision;
+            _canTri = canTrigger;
+            _canInter = canInteract;
+        }
 
-      private void OnTriggerEnter(Collider other)
-      {
-         if (!canTrigger) return;
+        public void Disable()
+        {
+            canCollision = false;
+            canTrigger = false;
+            canInteract = false;
+        }
 
-         OnEventTrigger(other.gameObject);
-      }
+        public void Enable()
+        {
+            canCollision = _cancol;
+            canTrigger = _canTri;
+            canInteract = _canInter;
+        }
 
-      private void OnCollisionExit(Collision other)
-      {
-         if (!canCollision || EventIsToggle) return;
+        #region Unity Collision Event
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!canCollision) return;
 
-         OnEventExit(other.gameObject);
-      }
+            colNormalVec = other.contacts[0].normal;
+            colVelocity = other.relativeVelocity;
+            colPosition = other.contacts[0].point;
 
-      private void OnTriggerExit(Collider other)
-      {
-         if (!canTrigger || EventIsToggle) return;
+            OnEventTrigger(other.gameObject);
+        }
 
-         OnEventExit(other.gameObject);
-      }
-      #endregion // Unity Collision Event
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!canTrigger) return;
 
-      public void Interact()
-      {
+            OnEventTrigger(other.gameObject);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (!canCollision || EventIsToggle) return;
+
+            OnEventExit(other.gameObject);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!canTrigger || EventIsToggle) return;
+
+            OnEventExit(other.gameObject);
+        }
+        #endregion // Unity Collision Event
+
+        public void Interact()
+        {
             OnEventTrigger(null);
-      }
-      public void Focus() { }
-      public void DeFocus() { }
-   }
+        }
+        public void Focus() { }
+        public void DeFocus() { }
+    }
 }
