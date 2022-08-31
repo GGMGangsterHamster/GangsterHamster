4fb5ba9639259944f89638218e324da8206c19af
JunseoJung <71419212+junff001@users.noreply.github.com>
2022-08-31 10:18:25 +0900

    만드는 중

--- AnotherGangsterHamster/Assets/_Scripts/Objects/Camera/CameraHeadBob.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Objects/Camera/CameraHeadBob.cs
@@ -2,51 +2,74 @@ using Characters.Player.Move;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Characters.Player;
 
 public class CameraHeadBob : MonoBehaviour
 {
-    private float _defaultHeight;
-    private float _maxHeight;
-    private float _minHeight;
-    private bool _isUP = true;
+    [SerializeField] private bool enable = true;
 
-    private MoveInputHandler _moveInputHandler;
+    [SerializeField, Range(0, 0.1f)] private float amplitude = 0.015f;
+    [SerializeField, Range(0, 30)] private float frequency = 10f;
+    
+    [SerializeField] private Transform cameraHolder;
+    [SerializeField] private Rigidbody player;
+
+    private Camera mainCam;
+    private float tpggleSpeed = 3f;
+    private Vector3 startPos;
+
+    void Awake()
+    {
+        startPos = mainCam.transform.localPosition;
+    }
 
     void Start()
     {
-        _defaultHeight = transform.localPosition.y;
-        _maxHeight = _defaultHeight + 0.05f;
-        _minHeight = _defaultHeight - 0.05f;
-
-        _moveInputHandler = GetComponentInParent<MoveInputHandler>();
+        mainCam = FindObjectOfType<Camera>();
     }
 
     void Update()
     {
-        if (!_moveInputHandler.IsIdle)
-        {
-            HeadBob();
-        }
+        if (!enable) return;
+
+        CheckMotion();
+        ResetPosition();
+        mainCam.transform.LookAt(FocusTarget());
     }
 
-    public void HeadBob()
+    private void PlayMotion(Vector3 motion)
     {
-        if (transform.localPosition.y >= _maxHeight)
-        {
-            _isUP = false;
-        }
-        else if(transform.localPosition.y <= _minHeight)
-        {
-            _isUP = true;
-        }
+        mainCam.transform.localPosition += motion;
+    }
 
-        if (_isUP)
-        {
-            transform.localPosition += new Vector3(0, Time.deltaTime * 0.5f);
-        }
-        else
-        {
-            transform.localPosition -= new Vector3(0, Time.deltaTime * 0.5f);
-        }
+    private void CheckMotion()
+    {
+        float speed = new Vector3(player.velocity.x, 0, player.velocity.z).magnitude;
+
+        if (speed < tpggleSpeed) return;
+        if (!PlayerStatus.OnGround) return;
+
+        PlayMotion(FootStepMotion());
+    }
+
+    private Vector3 FootStepMotion()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * frequency) * amplitude;
+        pos.x += Mathf.Cos(Time.time * frequency / 2) * amplitude * 2;
+        return pos;
+    }
+
+    private void ResetPosition()
+    {
+        if (mainCam.transform.localPosition == startPos) return;
+        mainCam.transform.localPosition = Vector3.Lerp(mainCam.transform.localPosition, startPos, 1 * Time.deltaTime);
+    }
+
+    private Vector3 FocusTarget()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + cameraHolder.localPosition.y, transform.position.z);
+        pos += cameraHolder.forward * 15f;
+        return pos;
     }
 }
