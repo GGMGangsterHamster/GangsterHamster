7af6e5286ea6cafb8694dc791c4f4d725c5417eb
HanUseGentoo <wooyeophan@gmail.com>
2022-08-31 15:16:24 +0900

    사운드메니저 Stop, Stopall

--- AnotherGangsterHamster/Assets/_Scripts/Sound/SoundManager.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Sound/SoundManager.cs
@@ -9,75 +9,98 @@ using UnityEngine.Networking;
 
 namespace Sound
 {
-   public class SoundManager : MonoSingleton<SoundManager>
-   {
-      private Dictionary<string, AudioClip> _audioDictionary;
-      private Dictionary<string, AudioSource> _curPlayingAudioSource;
-      public string soundEffectPath = "Audio/SoundEffect/";
-      public float GlobalVolume { get; set; } = 0.8f;
+    public class SoundManager : MonoSingleton<SoundManager>
+    {
+        private Dictionary<string, AudioClip> _audioDictionary;
+        private Dictionary<string, AudioSource> _curPlayingAudioSource;
+        public string soundEffectPath = "Audio/SoundEffect/";
+        public float GlobalVolume { get; set; } = 0.8f;
 
-      private void Start()
-      {
-         DontDestroyOnLoad(this.gameObject);
+        private void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
 
-         GenericPool
-            .Instance
-            .AddManagedObject<AudioSource>(this.transform,
-                                          (source) => {
-                                             source.playOnAwake = false;
-                                          }
-         );
+            GenericPool
+               .Instance
+               .AddManagedObject<AudioSource>(this.transform,
+                                             (source) =>
+                                             {
+                                                 source.playOnAwake = false;
+                                             }
+            );
 
-         _audioDictionary       = new Dictionary<string, AudioClip>();
-         _curPlayingAudioSource = new Dictionary<string, AudioSource>();
+            _audioDictionary = new Dictionary<string, AudioClip>();
+            _curPlayingAudioSource = new Dictionary<string, AudioSource>();
 
-         Resources.LoadAll<AudioClip>(soundEffectPath)
-                  .ToList()
-                  .ForEach(e => {
-                     _audioDictionary.Add(e.name, e);
-                     _curPlayingAudioSource.Add(e.name, null);
-                  }
-         );
-      }
+            Resources.LoadAll<AudioClip>(soundEffectPath)
+                     .ToList()
+                     .ForEach(e =>
+                     {
+                         _audioDictionary.Add(e.name, e);
+                         _curPlayingAudioSource.Add(e.name, null);
+                     }
+            );
+        }
 
-      /// <summary>
-      /// 오디오를 플레이 합니다.
-      /// </summary>
-      /// <param name="name"></param>
-      public void Play(string name, bool doNotPlayIfAlreadyPlaying = false)
-      {
-         if(!_audioDictionary.ContainsKey(name))
-         {
-            Logger.Log($"Cannot find audio {name}", LogLevel.Error);
-            return;
-         }
+        /// <summary>
+        /// 오디오를 플레이 합니다.
+        /// </summary>
+        /// <param name="name"></param>
+        public void Play(string name, bool doNotPlayIfAlreadyPlaying = false)
+        {
+            if (!_audioDictionary.ContainsKey(name))
+            {
+                Logger.Log($"Cannot find audio {name}", LogLevel.Error);
+                return;
+            }
 
-         if (_curPlayingAudioSource[name] != null &&
-             _curPlayingAudioSource[name].isPlaying && doNotPlayIfAlreadyPlaying)
-            return;
+            if (_curPlayingAudioSource[name] != null &&
+                _curPlayingAudioSource[name].isPlaying &&
+                doNotPlayIfAlreadyPlaying)
+                return;
 
-         AudioSource source = GenericPool
-                                 .Instance
-                                 .Get<AudioSource>(e => !e.isPlaying);
-         source.gameObject.SetActive(true);
-         source.clip   = _audioDictionary[name];
-         source.volume = GlobalVolume; // FIXME: TEMP
-         source.Play();
-         _curPlayingAudioSource[name] = source;
-      }
+            AudioSource source = GenericPool
+                                    .Instance
+                                    .Get<AudioSource>(e => !e.isPlaying);
+            source.gameObject.SetActive(true);
+            source.clip = _audioDictionary[name];
+            source.volume = GlobalVolume; // FIXME: TEMP
+            source.Play();
+            _curPlayingAudioSource[name] = source;
+        }
 
-       public void SetSound(float volume)
-       {
-          GlobalVolume = volume;
+        public void Stop(string name)
+        {
+            if (_curPlayingAudioSource.ContainsKey(name))
+                _curPlayingAudioSource[name].Stop();
+            else
+                Logger.Log($"SoundManager > Cannot found key: {name}",
+                    LogLevel.Error);
+        }
 
-          if(_curPlayingAudioSource != null && _curPlayingAudioSource.Count != 0)
-          {
-             foreach (AudioSource source in _curPlayingAudioSource.Values)
-             {
-                if(source != null && source.gameObject.activeSelf)
-                  source.volume = volume;
-             }
-          }
-       }
-   }
+        public void StopAll()
+        {
+            _curPlayingAudioSource
+                .Values
+                .ToList()
+                .ForEach(x => {
+                    x.Stop();
+                });
+        }
+
+        public void SetSound(float volume)
+        {
+            GlobalVolume = volume;
+
+            if (_curPlayingAudioSource != null &&
+                _curPlayingAudioSource.Count != 0)
+            {
+                foreach (AudioSource source in _curPlayingAudioSource.Values)
+                {
+                    if (source != null && source.gameObject.activeSelf)
+                        source.volume = volume;
+                }
+            }
+        }
+    }
 }
