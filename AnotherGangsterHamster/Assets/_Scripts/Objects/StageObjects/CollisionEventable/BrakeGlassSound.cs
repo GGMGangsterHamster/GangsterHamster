404ba1161d9cea1bb2f60da20da89a7a906a0e2f
JunseoJung <71419212+junff001@users.noreply.github.com>
2022-08-31 14:40:17 +0900

    유리 깨질때만 소리 나도록 수정

--- AnotherGangsterHamster/Assets/_Scripts/Objects/StageObjects/CollisionEventable/BrakeGlassSound.cs
+++ AnotherGangsterHamster/Assets/_Scripts/Objects/StageObjects/CollisionEventable/BrakeGlassSound.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sound;
+using Objects.StageObjects.CollisionEventable;
 
 public class BrakeGlassSound : SoundController
 {
-    public override void PlaySound(object obj)
+    private Glass glass;
+
+    void Start()
+    {
+        glass = GetComponent<Glass>();
+        glass.OnBreak.AddListener(BrakeGlass);
+    }
+
+    void BrakeGlass()
     {
         SoundManager.Instance.Play("GlassBrake");
+    }
+
+    public override void PlaySound(object obj)
+    {
+       
     }
 }
