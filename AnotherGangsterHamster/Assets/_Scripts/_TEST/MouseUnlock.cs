55285c584cec344d078e5a1802b75bc213d860f0
김민준 <eppurdream@gmail.com>
2022-10-11 23:30:04 +0900

    엔딩 후 생기는 오류 수정함

---
+++ AnotherGangsterHamster/Assets/_Scripts/_TEST/MouseUnlock.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseUnlock : MonoBehaviour
+{
+    public void Unlock()
+    {
+        Utils.UnlockCursor();
+    }
+}
